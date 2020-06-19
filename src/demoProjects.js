a5cb663906175151887f50a2c07a8e2f0559f1a1
fiireball <adrian.fiire@gmail.com>
2020-06-19 16:52:14 +0200

    fixed demo object content

--- src/demoProjects.js
+++ src/demoProjects.js
@@ -7,8 +7,8 @@ const demo = (() => {
   const toDo2 = taskFactory('Drink Water', 'Have to drink water to stay hydrated', '19.06.2020', 'high', 'no notes', ['Check1', 'Check2', 'check3'], false);
   const project1 = projectFactory('Food', 'All things food related', 'red');
 
-  const toDo3 = taskFactory('Pee', 'Empty your collon', '19.06.2020', 'high', 'no notes', ['Check1', 'Check2', 'check3'], false);
-  const toDo4 = taskFactory('Poop', 'Empty you bladder', '19.06.2020', 'high', 'no notes', ['Check1', 'Check2', 'check3'], false);
+  const toDo3 = taskFactory('Pee', 'Empty you bladder', '19.06.2020', 'high', 'no notes', ['Check1', 'Check2', 'check3'], false);
+  const toDo4 = taskFactory('Poop', 'Empty your collon', '19.06.2020', 'high', 'no notes', ['Check1', 'Check2', 'check3'], false);
   const project2 = projectFactory('Bio stuff', 'all things bathroom related');
 
   project1.addTask(toDo1);
