856f17b6017c0a2c98644e041905ecd96d191ac5
fiireball <adrian.fiire@gmail.com>
2020-06-19 13:47:00 +0200

    rendering projects test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+/* eslint-disable import/extensions */
+import pubSub from './pubsub.js';
+import taskFactory from './task.js';
+import projectFactory from './project.js';
+import DOMController from './DOMController.js'
+import demo from './demoProjects.js'
+
 console.log('webpack init');
+
+const projects = demo.getProjects();
+
+DOMController.renderProjects(projects);
+
+
+projects.forEach((project) => {
+  if (project.IsActive()) {
+    DOMController.renderTasks(project);
+  }
+})
